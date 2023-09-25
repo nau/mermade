579260c608563e760ccf3d9b8281420311182e78
Alexander Nemish <anemish@gmail.com>
2023-09-26 00:46:17 +0200

    download_file

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_files::NamedFile;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
 use env_logger;
 use std::env;
 
@@ -6,7 +7,10 @@ use crate::merkle;
 use actix_multipart::Multipart;
 use futures::{StreamExt, TryStreamExt};
 use serde::Serialize;
+use std::fs::File;
+use std::io::Read;
 use std::io::Write;
+use std::path::PathBuf;
 
 #[derive(Serialize)]
 struct Message {
@@ -46,6 +50,15 @@ async fn upload_file(mut payload: Multipart) -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+#[get("/download/{fileindex}")]
+async fn download_file(path: web::Path<String>) -> Result<NamedFile> {
+    println!("Downloading file {}", path);
+    let filename = path.into_inner();
+    // TODO: ensure that it's impossible to download files outside of the current directory
+    let named_file = NamedFile::open(&filename)?;
+    Ok(named_file)
+}
+
 #[actix_web::main]
 pub async fn server() -> std::io::Result<()> {
     env_logger::Builder::new()
@@ -55,62 +68,13 @@ pub async fn server() -> std::io::Result<()> {
 
     let server = HttpServer::new(|| {
         App::new()
+            .service(download_file)
             .route("/upload", web::post().to(upload_file))
+            // .route("/download", web::get().to(download_file))
             .route("/", web::get().to(hello))
     });
 
     log::info!("Starting server at 0.0.0.0:8080");
 
     server.bind("0.0.0.0:8080")?.run().await
-}
-
-fn show_usage() {
-    println!("Usage: merkle-tree <command> [args]");
-    println!("Commands:");
-    println!("  server -- will start the server");
-    println!("  upload <server ip address> -- will upload all files in the current directory to the server");
-    println!("  download <server ip address> <index> -- will download the file with the given index from the server");
-    println!("  verify <server ip address> <index> -- will verify the downloaded file with the given index from the server");
-}
-
-fn upload_files(server_ip: &str, dir: &str) {
-    todo!()
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(&args);
-    if args.len() == 2 {
-        let command = &args[1];
-        match command.as_str() {
-            "server" => {
-                println!("Starting server...");
-                let _ = server();
-            }
-            _ => show_usage(),
-        }
-    } else if args.len() == 3 {
-        let command = &args[1];
-        match command.as_str() {
-            "upload" => {
-                let server_ip = &args[2];
-                println!("Uploading files to {}", server_ip);
-                upload_files(server_ip, ".");
-            }
-            "download" => {
-                // parse integer from args
-                let file_index = args[2].parse::<usize>().unwrap();
-                // TODO
-            }
-            "verify" => {
-                let file_index = args[2].parse::<usize>().unwrap();
-                // TODO
-            }
-            _ => show_usage(),
-        }
-    } else {
-        show_usage();
-    }
-
-    // let _ = server();
 }
