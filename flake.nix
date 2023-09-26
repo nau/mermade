54cef700606b4344d7d4035ca1f45548ef2e0b3f
Alexander Nemish <anemish@gmail.com>
2023-09-26 22:56:08 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
   outputs = { self, nixpkgs }: {
     devShell.x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.mkShell {
       buildInputs = with nixpkgs.legacyPackages.x86_64-darwin; [
+        darwin.apple_sdk.frameworks.Security
+        zlib
         rustc
         cargo
         rustfmt
